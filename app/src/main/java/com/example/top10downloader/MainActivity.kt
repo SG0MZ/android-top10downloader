9fa1efaedf2b5df6885045460f06f436a04e18ae
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-05 14:47:50 -0600

    AsyncTask - First Commit

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.top10downloader
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"onCreate called")
+    }
+
+    private inner class DownloadData : AsyncTask<String, Void, String>() {
+        private val TAG = "Download"
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun doInBackground(vararg p0: String?): String {
+            TODO("Not yet implemented")
+        }
     }
 }
